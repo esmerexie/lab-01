6748e1c725b2ed7696382d18c186f863dbd41f3e
Rexie Esmeralda <esmeraldarexie@gmail.com>
2022-07-12 20:54:40 -0700

    did some initial refactoring of code

--- src/App.js
+++ src/App.js
@@ -10,10 +10,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <img src={Cow} alt='cow' height={200} width={200}/>
-      <Main />
-      <img src={Goat} alt='goat'height={200} width={200}/>
-      <Main />
+      <Main img={Cow} title="Horned Beast" website="https://www.pexels.com/search/animals%20with%20horns/" description="this is a cow! moo!"/>
+      <Main img={Goat} title="Another Horned Beast" website="https://www.pexels.com/search/animals%20with%20horns/" description="this is a goat"/>
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-
 class Footer extends Component {
     constructor(){
         super()
